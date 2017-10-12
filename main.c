1681b196fb85d4af275935c3099766e2468187b6
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-12 23:05:23 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 12 ottobre 2017, 22.37
- */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /*
+ * Funzione che calcola il logaritmo in base b di x
  * 
+ * @param b Base del logaritmo
+ * @param x Argomento del logaritmo
+ * @return Valore del logaritmpo: logb(x)
  */
-int main(int argc, char** argv) {
+double logaritmo(double b, double x);
 
+int main(int argc, char** argv) {
+    double b = 2;
+    double x = 8;
+    
+    double risultato = logaritmo(b,x);
+    
+    printf("Il logaritmo in base %g di %g e': %g",b,x,risultato);
+    
     return (EXIT_SUCCESS);
 }
 
+double logaritmo(double b, double x){
+    double result;
+    
+    result = (log(x)/log(b));   //formula del cambio di base
+    
+    return result;
+}
