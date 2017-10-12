cb0344ee13c118a199499a8e246b9cecb8bbb5e5
Nicolò Vescera <nicolo.vescera@gmail.com>
2017-10-12 23:18:19 +0200

    Completato esercizio

--- main.c
+++ main.c
@@ -1,18 +1,66 @@
-/* 
- * File:   main.c
- * Author: ncvescera
- *
- * Created on 12 ottobre 2017, 22.37
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 /*
+ * Funzione che dato in ingresso un numero compresto tra 1 e 12 stampa il mese dell'anno corrispondente
  * 
+ * @param number numero del mese
  */
-int main(int argc, char** argv) {
+void numberToMounth(int number);
 
+int main(int argc, char** argv) {
+    int number;
+    
+    printf("Inserisci un numero compreso tra 1 e 12\n");
+    scanf("%d",&number);
+    
+    numberToMounth(number);
+    
     return (EXIT_SUCCESS);
 }
 
+void numberToMounth(int number){
+    switch(number){
+        case 1:
+            printf("Gennaio");
+            break;
+        case 2:
+            printf("Febbraio");
+            break;
+        case 3:
+            printf("Marzo");
+            break;
+        case 4:
+            printf("Aprile");
+            break;
+        case 5:
+            printf("Maggio");
+            break;
+        case 6:
+            printf("Giugno");
+            break;
+        case 7:
+            printf("Luglio");
+            break;
+        case 8:
+            printf("Agosto");
+            break;
+        case 9:
+            printf("Settembre");
+            break;
+        case 10:
+            printf("Ottobre");
+            break;
+        case 11:
+            printf("Novembre");
+            break;
+        case 12:
+            printf("Dicembre");
+            break;
+        default:
+            printf("Il numero deve essere compresto tra 1 e 12");
+            break;
+    }
+    
+    return;
+}
